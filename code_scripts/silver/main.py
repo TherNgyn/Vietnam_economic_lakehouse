9ee849392a34c775eb7e58d19936047b4e68950f
Doan Quang Lam <161808993+lamdoan204@users.noreply.github.com>
2026-07-04 00:38:29 +0700

    xong test dags

--- code_scripts/silver/main.py
+++ code_scripts/silver/main.py
@@ -49,16 +49,12 @@ def main_func():
 
 
         extract_data_for_Product_Productivity_fact(excel_file, year, month)
-
-        # Giải phóng bộ nhớ RAM của file hiện tại trước khi xử lý file tiếp theo
         del excel_file
-        gc.collect()
+        # Giải phóng bộ nhớ RAM của file hiện tại trước khi xử lý file tiếp theo
     
     print('BẮT ĐẦU TRÍCH XUẤT DỮ LIỆU INVESTMENT BY SECTOR')
     excel_file = get_investment_by_sector_raw_data()
     extract_data_from_Investment_by_Sector(excel_file)
-
-      
-    print(f"Tải thành công dữ liệu từ file: tháng: {month} - năm: {year} lên SILVER LAYER")
+    del excel_file
 
 main_func()
